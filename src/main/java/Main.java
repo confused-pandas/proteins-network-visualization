d4eb412fb38713b57e83a5f7f0e8967a77d473bd
Maxime Guyot <Maxime.Guyot.1@telecomnancy2.eu>
2018-11-19 09:46:05 +0100

    Changements sur le main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -5,7 +7,8 @@ import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 public class Main {
     public static void main(String[] args) {
         GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-        GraphDatabaseService db = dbFactory.newEmbeddedDatabase("C:/TPNeo4jDB");
+        File p = new File("./graph.db");
+        GraphDatabaseService db = dbFactory.newEmbeddedDatabase(p);
 
         try (Transaction tx = db.beginTx()) {
             // Perform DB operations
