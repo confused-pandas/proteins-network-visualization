ab2ad054c3bee2183f1bdd576901f6afef3d4c3d
Najib Aghenda <aghenda1u@tncy-04-03.telecomnancy.site.univ-lorraine.fr>
2018-11-29 13:52:49 +0100

    Changement des fichiers, ajout des GO

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,15 +42,15 @@ public class Main {
 			}
 			*/
 			
-			// GET DOMAINS
+			// GET EC
 			Map<String, Object> params = new HashMap<>();
-			String query = "MATCH (n:Protein) RETURN n.domains;";
+			String query = "MATCH (n:Protein) RETURN n.goterms;";
 			Result result = graphDb.execute(query, params);
 			System.out.println(result);
 		
 			while(result.hasNext()) {
 				Map<String, Object> row = result.next();
-				System.out.println(row.get("n.domains"));
+				System.out.println(concatFromArray(((String[]) row.get("n.goterms"))));
 			}
 			
 			
@@ -57,6 +58,17 @@ public class Main {
         }
 		
 		graphDb.shutdown();
+	}
+	
+	static String concatFromArray(String[] a) {
+		if (a == null) {
+			return "";
+		}
+		String result="";
+		for (String i : a) {
+			result += i + " ";
+		}
+		return result;
 	}
 
 }
