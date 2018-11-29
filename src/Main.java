43ef85c2edf45b648efe141395e0c789c32ee105
Najib Aghenda <aghenda1u@tncy-04-03.telecomnancy.site.univ-lorraine.fr>
2018-11-29 09:38:08 +0100

    ça marche

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
@@ -9,11 +12,23 @@ public class Main {
 	public static void main(String[] args) {
 		
 		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-		GraphDatabaseService graphDb = dbFactory.newEmbeddedDatabase( new File("./resources/graph.db") );
+		GraphDatabaseService graphDb = dbFactory.newEmbeddedDatabase( new File("/Users/aghenda1u/Downloads/neo4j-community-3.4.10/data/databases/graph.db") );
 		
 		try ( Transaction tx = graphDb.beginTx() )
         {
             // Database operations go here
+			Map<String, Object> params = new HashMap<>();
+			String query = "MATCH (n:Protein) RETURN n.id;";
+			Result result = graphDb.execute(query, params);
+			System.out.println(result);
+		
+			while(result.hasNext()) {
+				Map<String, Object> row = result.next();
+				System.out.println(row.get("n.id"));
+			}
+		
+			
+			
             tx.success();
         }
 		
