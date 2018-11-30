d57d0109c4646358f1ad0fe0de8f80c2b75afb99
STUNAULT Guillaume <guillaume.stunault@telecomnancy.eu>
2018-11-30 17:52:17 +0100

    Ajout interface

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,79 @@
+
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
-public class Main {
+public class Main extends Application {
 
-	public static void main(String[] args) {
-		
+	private static Pane root ;
+
+
+	@FXML
+	private TextField protID;
+	@FXML
+	private ListView ecnumbers;
+	@FXML
+	private ListView goterms;
+	@FXML
+	private ListView domains;
+	@FXML
+	private ListView neighbours;
+	@FXML
+	private ListView stats;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		root = FXMLLoader.load(getClass().getResource("Interface.fxml"));
+		primaryStage.setTitle("DrugMediator");
+		primaryStage.setScene(new Scene(root, 900, 600));
+		primaryStage.show();
+	}
+
+	@FXML
+	private void runapp(ActionEvent e) throws IOException {
 		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-		GraphDatabaseService graphDb = dbFactory.newEmbeddedDatabase( new File("/home/etudiants/aghenda1u/Documents/NoSQL/neo4j-community-3.4.10/data/databases/graph.db") );
-		
+		GraphDatabaseService graphDb = dbFactory.newEmbeddedDatabase( new File("C:\\neo4j\\neo4j-community-3.4.10\\data\\databases/graph.db") );
+
+		Main.rr(graphDb);
+
+		graphDb.shutdown();
+	}
+
+	private static void rr(GraphDatabaseService graphDb) throws  IOException{
+		Map<String, Object> params = new HashMap<>();
+		String query = "MATCH (n:Protein) RETURN n.domains;";
+		Result result = graphDb.execute(query, params);
+		System.out.println(result);
+
+		while(result.hasNext()) {
+			Map<String, Object> row = result.next();
+			System.out.println(concatFromArray(((String[]) row.get("n.domains"))));
+		}
+	}
+
+
+	public static void main(String[] args) throws IOException {
+
+/*
 		try ( Transaction tx = graphDb.beginTx() )
         {
             
-			/*
+
 			// Database operations go here
 			Map<String, Object> params = new HashMap<>();
 			String query = "MATCH (n:Protein) RETURN n.id;";
@@ -40,35 +95,28 @@ public class Main {
 				Map<String, Object> row = result.next();
 				System.out.println(row.get("n.ec"));
 			}
-			*/
+
 			
 			// GET EC
-			/*Map<String, Object> params = new HashMap<>();
-			String query = "MATCH (p:Protein) RETURN p.domains;";
-			Result result = graphDb.execute(query, params);
-			System.out.println(result);		
-		
-			while(result.hasNext()) {
-				Map<String, Object> row = result.next();
-				System.out.println(concatFromArray(((String[]) row.get("p.domains"))));
-			}*/
-			
-			
+
 			Map<String, Object> params = new HashMap<>();
-			String query = "MATCH (p:Protein)-[r:weight]-(p2) where p.id = \"P08393\" return p,r,p2.id";
+			String query = "MATCH (n:Protein) RETURN n.domains;";
 			Result result = graphDb.execute(query, params);
-			System.out.println(result);		
+			System.out.println(result);
 		
 			while(result.hasNext()) {
 				Map<String, Object> row = result.next();
-				System.out.println(row.get("p2.id"));
+				System.out.println(concatFromArray(((String[]) row.get("n.domains"))));
 			}
-			
+
 			
             tx.success();
         }
 		
 		graphDb.shutdown();
+	*/
+
+		launch(args);
 	}
 	
 	static String concatFromArray(String[] a) {
@@ -81,5 +129,7 @@ public class Main {
 		}
 		return result;
 	}
+
+
 
 }
